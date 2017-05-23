164a76bcd4819ef42f347443f813377d3836f2ff
ibongiovanni <ignaciobongiovanni@gmail.com>
2017-05-23 17:10:46 -0300

    reduce code lines in semaphores implementation

--- xv6-unrc/semaphore.c
+++ xv6-unrc/semaphore.c
@@ -6,74 +6,89 @@
 #include "mmu.h" //It is necessary to include it to include proc.h?
 #include "proc.h"
 
+//atomice release and return code process
+#define relandret(c) \
+  release(&semtable.lock); \
+  return c
+
 struct {
   struct spinlock lock;
   struct semaphore sem[MAXSEM];
 } semtable;
 
-//Creates or retun a semaphores descriptor
+//Search in proc->osems if any points to same as 's'
+//return arrays position if found, otherwise -1 
+static int
+search_in_process_osems(struct semaphore *s){
+  int id;
+  for (id = 0; id < MAXSEMPROC && proc->osem[id] != s; ++id){
+    //nothing, just skip
+  }
+  if(id==MAXSEMPROC){ //s not found in osems
+    return -1;
+  }
+  return id;
+}
+
+/*
+//Search a sem with id = sem_id in semtable
+//returns pointer to sem if found, otherwise null
+static struct semaphore *
+search_in_semtable(int sem_id){
+  struct semaphore *s;
+  for(s = semtable.sem; s < &semtable.sem[MAXSEM]; s++){
+    if(s->id == sem_id){ //Found sem
+      return s;
+    }
+  }
+  return 0;
+}
+*/
+
+//Creates or return a semaphores descriptor
 //Returns semaphore id or error code
 int
 semget(int sem_id, int init_value){
   int id;
   struct semaphore *s;
   acquire(&semtable.lock);
-  if (sem_id < -1 || sem_id == 0) { //Invalid sem_id argument
-    release(&semtable.lock);
-    return -4;
+  if (sem_id < -1 || sem_id >= MAXSEM) { //Invalid sem_id argument
+    relandret(-4);
   } 
   
   if (sem_id == -1) { //Get new semaphore
     //Search empty place in process semaphores
-    for (id = 0; id < MAXSEMPROC && proc->osem[id]!=0; ++id){
-      //nothing, just skip
-    }
-    if(id==MAXSEMPROC){ //process has no free place
-      release(&semtable.lock);
-      return(-2);
+    id = search_in_process_osems(0);
+    if (id<0){  //process has no free place
+      relandret(-2);
     }
     //Search empty place in semtable
     for(s = semtable.sem; s < &semtable.sem[MAXSEM]; s++){
       if(s->refcount==0){ //Found free sem
         //init semaphore and vinculate to process
-        s->id = s-semtable.sem+1; //plus 1 to avoid id=0
+        s->id = s-semtable.sem; //sem id is array position
         s->refcount++;
         s->val = init_value;
         proc->osem[id] = s;
-        release(&semtable.lock);
-        return(s->id);
+        relandret(s->id);
       }
     }
     //No free sem in system
-    release(&semtable.lock);
-    return(-3);
+    relandret(-3);
   }
-  else{ //Search for sem_id
-    for(s = semtable.sem; s < &semtable.sem[MAXSEM]; s++){
-      if(s->id == sem_id){ //Found sem
-        if(s->refcount>0){
-          //Search empty place in process semaphores
-          for (id = 0; id < MAXSEMPROC && proc->osem[id]!=0; ++id){
-            //nothing, just skip
-          }
-          if(id==MAXSEMPROC){ //process has no free place
-            release(&semtable.lock);
-            return(-2);
-          }
-          //vinculate semaphore to process
-          s->refcount++;
-          proc->osem[id] = s;
-          release(&semtable.lock);
-          return(s->id); //exit on succes
-        }
-        //Sem_id is not in use
-        release(&semtable.lock);
-        return(-1);
-      }
+  else{ //Get semaphore in use
+    s = &semtable.sem[sem_id];
+    if (s->refcount==0){ //sem_id is not in use
+      relandret(-1);
     }
-    //Sem_id not found
-    release(&semtable.lock);
-    return(-1);
+    id = search_in_process_osems(0);
+    if (id<0){  //process has no free place
+      relandret(-2);
+    }
+    //vinculate semaphore to process
+    s->refcount++;
+    proc->osem[id] = s;
+    relandret(s->id); //exit on succes
   }
 }
 
@@ -84,31 +99,18 @@ semfree(int sem_id){
   struct semaphore *s;
   acquire(&semtable.lock);
   //Search for sem_id
-  for(s = semtable.sem; s < &semtable.sem[MAXSEM]; s++){
-    if(s->id == sem_id){ //Found sem
-      if(s->refcount>0){ //sem is in use
-        //Search if the process has the semaphore
-        for (id = 0; id < MAXSEMPROC && proc->osem[id] != s; ++id){
-          //nothing, just skip
-        }
-        if(id==MAXSEMPROC){ //process is not vinculated to semaphore
-          release(&semtable.lock);
-          return(-1);
-        }
-        //decrement ref count and devinculate process
-        s->refcount--;
-        proc->osem[id] = 0;
-        release(&semtable.lock);
-        return(0); //exit on succes
-      }
-      //Sem_id is not in use
-      release(&semtable.lock);
-      return(-1);
-    }
+  s = &semtable.sem[sem_id];
+  if (s->refcount==0){ //sem_id is not in use
+    relandret(-1);
   }
-  //Sem_id not found
-  release(&semtable.lock);
-  return(-1);
+  id = search_in_process_osems(s);
+  if (id<0){  //process is not vinculated to semaphore
+    relandret(-1);
+  }
+  //decrement ref count and devinculate process
+  s->refcount--;
+  proc->osem[id] = 0;
+  relandret(0); //exit on succes
 }
 
 //Decrement semaphore value
@@ -117,34 +119,20 @@ semdown(int sem_id){
   int id;
   struct semaphore *s;
   acquire(&semtable.lock);
-  //Search for sem_id
-  for(s = semtable.sem; s < &semtable.sem[MAXSEM]; s++){
-    if(s->id == sem_id){ //Found sem
-      if(s->refcount>0){
-        //Search if the process has the semaphore
-        for (id = 0; id < MAXSEMPROC && proc->osem[id] != s; ++id){
-          //nothing, just skip
-        }
-        if(id==MAXSEMPROC){ //process is not vinculated to semaphore
-          release(&semtable.lock);
-          return(-1);
-        }
-        //decrement sem val or lock caller process
-        while(s->val < 1){
-          sleep(&semtable, &semtable.lock);
-        }
-        s->val--;
-        release(&semtable.lock);
-        return(0); //exit on sucess
-      }
-      //Sem_id is not in use
-      release(&semtable.lock);
-      return(-1);
-    }
+  s = &semtable.sem[sem_id];
+  if (s->refcount==0){ //sem_id is not in use
+    relandret(-1);
   }
-  //Sem_id not found
-  release(&semtable.lock);
-  return(-1);
+  id = search_in_process_osems(s);
+  if (id<0){  //process is not vinculated to semaphore
+   relandret(-1);
+  }
+  //decrement sem val or lock caller process
+  while(s->val < 1){
+    sleep(&semtable, &semtable.lock);
+  }
+  s->val--;
+  relandret(0); //exit on sucess
 }
 
 //Increment semaphore value
@@ -153,30 +141,16 @@ semup(int sem_id){
   int id;
   struct semaphore *s;
   acquire(&semtable.lock);
-  //Search for sem_id
-  for(s = semtable.sem; s < &semtable.sem[MAXSEM]; s++){
-    if(s->id == sem_id){ //Found sem
-      if(s->refcount>0){
-        //Search if the process has the semaphore
-        for (id = 0; id < MAXSEMPROC && proc->osem[id] != s; ++id){
-          //nothing, just skip
-        }
-        if(id==MAXSEMPROC){ //process is not vinculated to semaphore
-          release(&semtable.lock);
-          return(-1);
-        }
-        //increment sem val and wake up sleeping process
-        s->val++;
-        wakeup(&semtable);
-        release(&semtable.lock);
-        return(0); //exit on sucess
-      }
-      //Sem_id is not in use
-      release(&semtable.lock);
-      return(-1);
-    }
+  s = &semtable.sem[sem_id];
+  if (s->refcount==0){ //sem_id is not in use
+    relandret(-1);
   }
-  //Sem_id not found
-  release(&semtable.lock);
-  return(-1);
+  id = search_in_process_osems(s);
+  if (id<0){  //process is not vinculated to semaphore
+    relandret(-1);
+  }
+  //increment sem val and wake up sleeping process
+  s->val++;
+  wakeup(&semtable);
+  relandret(0); //exit on sucess
 }
--- xv6-unrc/semaphore.h
+++ xv6-unrc/semaphore.h
@@ -1,6 +1,6 @@
 
 struct semaphore {
-  uint id; //semaphore id : [1..MAXSEM]
+  uint id; //semaphore id : [0..MAXSEM-1]
   int val; //semaphore value
   int refcount; //n of process referencing to the semaphore
 
