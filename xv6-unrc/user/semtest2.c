32dbd6ca7738d2bbc9df0325fd0a3906cd808209
ibongiovanni <ignaciobongiovanni@gmail.com>
2017-05-24 18:27:42 -0300

    update user test for semaphores

--- xv6-unrc/user/semtest2.c
+++ xv6-unrc/user/semtest2.c
@@ -8,36 +8,43 @@
 
 int main(int argc, char const *argv[])
 {
-  int fd,i,n,fid;
+  int fd,fd2,i,l,fid;
   char path[] = "fsem";
   int sem;
-  char r[2];
-  char a[] = "cc";
+
+  char buf[512];
+  char a[] = "0";
   printf(1,"a = %s\n",a );
-  memset(r, 0, sizeof(r));
 
   sem = semget(-1,1);
   if (sem<0) exit();
   printf(1,"semget ok\n");
   fd = open(path, O_CREATE | O_RDWR);
+  fd2 = open(path, O_RDONLY);
   if (fd<0) exit();
-  printf(1,"open ok, fd=%d\n",fd);
-  n=0;
+  printf(1,"open ok, fd=%d, fd2=%d\n",fd,fd2);
+  
   if ( write(fd, a, sizeof(a)) < 0) exit();
   printf(1,"write ok\n");
-
   fid = fork();
+  if(fid==0) semget(sem,0);
   for(i=0; i < MAX; i++){
-      if ( read(fd,r,sizeof(r)) < 0) exit();
-      printf(1,"readed '%s'\n",r );
-      n = atoi(r);
-      n++;
-      write(fd,&n,sizeof(n));
+    if (fid == 0) printf(1,"child ");
+    else          printf(1,"father ");
+    if ( semdown(sem) < 0) exit();
+    //printf(1,"semdown ok\n" );
+    l=read(fd2,buf,sizeof(buf));
+    if ( l < 0) exit();
+    printf(1,"readed %d bytes :'%s'\n",l,buf );
+
+    write(fd,buf,l);
+    if ( semup(sem) < 0) exit();
+    //printf(1,"semup ok\n" );
   }
   if(fid==0) exit();
   
   wait();
-  printf(1,"n= %d\n",n);
+
   if(semfree(sem)<0) exit();
   printf(1,"semfree ok\n");
   exit();
