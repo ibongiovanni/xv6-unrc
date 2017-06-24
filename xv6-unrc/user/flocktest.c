8df5b6034b393e147d4c332d68328c600adab6d2
ibongiovanni <ignaciobongiovanni@gmail.com>
2017-06-24 18:53:31 -0300

    update user test for flock and funlock

--- xv6-unrc/user/flocktest.c
+++ xv6-unrc/user/flocktest.c
@@ -6,29 +6,34 @@
 
 void
 locktest(int fd){
-  int i,n,l;
+  int i,n;
   char buf[512];
-  for (i = 0; i < 10; ++i)
+  for (i = 0; i < 100; ++i)
   {
     
     flock(fd);
-
+    //printf(1,"pid:%d enter critic zone\n",getpid());
     if ( fseek(fd, 0) < 0) exit();
-    l=read(fd, buf, sizeof(buf));
-    printf(1,"pid:%d readed:%d bytes\n",getpid(),l);
+    read(fd, buf, sizeof(buf));
+    //printf(1,"pid:%d readed:%d bytes\n",getpid(),l);
     n = atoi(buf);
 
     if ( fseek(fd, 0) < 0) exit();
     printf(fd, "%d",++n);
     if ( fseek(fd, 0) < 0) exit();
+    //printf(1,"pid:%d exit critic zone\n\n",getpid());  
     funlock(fd);
   }
 }
 
 int main(int argc, char const *argv[])
 {
-  int fd,pid;
-  fd = open("count", O_CREATE | O_RDWR);
+  int fd,pid,count;
+  char buf[512];
+  char *path; 
+  if(argc>1) path=(char*)argv[1];
+  else path="count";
+  fd = open(path, O_CREATE | O_RDWR);
   printf(fd, "%d",0);
   pid=0;
   /*while(i<3 && pid==0){
@@ -40,6 +45,11 @@ int main(int argc, char const *argv[])
   locktest(fd);
   if(pid==0) exit();
   wait();
+
+  if ( fseek(fd, 0) < 0) exit();  
+  read(fd, buf, sizeof(buf));
+  count = atoi(buf);
+  printf(1,"count = %d\n", count);
   close(fd);
   exit();
 }
