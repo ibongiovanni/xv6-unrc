feb5129a0573308a5ce009d22eb62c874b45502f
ibongiovanni <ignaciobongiovanni@gmail.com>
2017-06-30 11:13:44 -0300

    update user program rename

--- xv6-unrc/user/rename.c
+++ xv6-unrc/user/rename.c
@@ -9,8 +9,9 @@
 int main(int argc, char const *argv[])
 {
   char *oldpath,*newpath;
+  int ec;
 
-  if (argc < 3)
+  if (argc != 3)
   {
     printf(2,"usage: 'rename oldpath newpath'\n" );
     exit();
@@ -18,14 +19,12 @@ int main(int argc, char const *argv[])
   oldpath=(char*)argv[1];
   newpath=(char*)argv[2];
 
-  if(open(oldpath, O_RDONLY) < 0){
-    printf(2,"error: %s does not exist.\n",oldpath );
+  ec=rename(oldpath, newpath);
+  if( ec < 0){
+    printf(2,"rename: ");
+    if(ec==-1) printf(2,"'%s' does not exist.\n",oldpath);
+    if(ec==-2) printf(2,"cannot move '%s' to '%s'.\n",oldpath,newpath);
     exit();
   }
-  if(link(oldpath,newpath) < 0){
-    printf(2,"error: cannot link to %s.\n",newpath );
-    exit();
-  }
-  unlink(oldpath);
   exit();
 }
